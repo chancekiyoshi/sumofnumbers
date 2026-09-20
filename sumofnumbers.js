9c98ad58c49a494bfdf8552bbb463e95a5dc0084
Chance Kiyoshi <kiyoshichance@gmail.com>
2026-09-19 22:56:59 -1000

    Complete sumofnumbers practice quiz
    sumofnumbers Quiz

---
+++ sumofnumbers.js
@@ -0,0 +1,30 @@
+"use strict";
+function sumFor(numbers) {
+    let total = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        total += numbers[i];
+    }
+    return total;
+}
+function sumWhile(numbers) {
+    let total = 0;
+    let i = 0;
+    while (i < numbers.length) {
+        total += numbers[i];
+        i++;
+    }
+    return total;
+}
+function sumRecursion(numbers) {
+    if (numbers.length === 0) {
+        return 0;
+    }
+    return numbers[0] + sumRecursion(numbers.slice(1));
+}
+function sumTheFunctionalWay(numbers) {
+    return numbers.reduce((acc, curr) => acc + curr, 0);
+}
+console.log(sumFor([1, 2, 3, 4]));
+console.log(sumWhile([1, 2, 3, 4]));
+console.log(sumRecursion([1, 2, 3, 4]));
+console.log(sumTheFunctionalWay([1, 2, 3, 4]));
